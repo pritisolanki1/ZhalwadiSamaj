d4c76c7d5506eac5d99f5fcbf2e819b24042b851
Sunil Solanki <sns@taglineinfotech.com>
2026-05-29 12:54:05 +0530

    Enhanced forget password API

--- app/Helpers/generalHelper.php
+++ app/Helpers/generalHelper.php
@@ -4,6 +4,7 @@ use App\Models\Member;
 use App\Models\Report;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 use LaravelFCM\Facades\FCM;
 use LaravelFCM\Message\Exceptions\InvalidOptionsException;
 use LaravelFCM\Message\OptionsBuilder;
@@ -114,65 +115,71 @@ function makeLastImageValueSet($iData): bool|array|string
 
 function sendNotice($title, $body, $data = [], $user = [], $exclude_user = []): void
 {
-    Log::info('FCM enter');
-    $optionBuilder = new OptionsBuilder();
     try {
-        $optionBuilder->setTimeToLive(60 * 20);
-    } catch (InvalidOptionsException $e) {
-    }
-
-    $notificationBuilder = new PayloadNotificationBuilder($title);
-    $notificationBuilder->setBody($body)->setSound('default');
-
-    $dataBuilder = new PayloadDataBuilder();
-    $dataBuilder->addData($data);
-
-    $option = $optionBuilder->build();
-    $notification = $notificationBuilder->build();
-    $data = $dataBuilder->build();
-
-    // You must change it to get your tokens
-    $tokens = Member::select('device_token');
-
-    if (!empty($user)) {
-        $tokens = $tokens->whereIn('id', $user);
-    }
-
-    if (!empty($exclude_user)) {
-        $tokens = $tokens->whereNotIn('id', $user);
-    }
-
-    $tokens = $tokens->whereNotNull('device_token')->where(
-        'device_token',
-        '!=',
-        ''
-    )->groupBy('device_token')->get()->pluck('device_token')->toArray();
-
-    if (!empty($tokens)) {
-        Log::info('FCM total token count', [count($tokens)]);
-        $downstreamResponse = FCM::sendTo($tokens, $option, $notification, $data);
-        Log::info('FCM in number of success', [$downstreamResponse->numberSuccess()]);
-        Log::error('FCM in number of failure', [$downstreamResponse->numberFailure()]);
-        $downstreamResponse->numberModification();
-
-        // return Array - you must remove all this tokens in your database
-        Member::whereIn('device_token', $downstreamResponse->tokensToDelete())->update(['device_token' => null]);
-
-        // return Array (key : oldToken, value : new token - you must change the token in your database)
-        $downstreamResponse->tokensToModify();
-
-        // return Array - you should try to resend the message to the tokens in the array
-        if (!empty($downstreamResponse->tokensToRetry())) {
-            FCM::sendTo($downstreamResponse->tokensToRetry(), $option, $notification, $data);
+        Log::info('FCM enter');
+        $optionBuilder = new OptionsBuilder();
+        try {
+            $optionBuilder->setTimeToLive(60 * 20);
+        } catch (InvalidOptionsException $e) {
         }
 
-        // return Array (key:token, value:error) - in production you should remove from your database the tokens present in this array
-        if (!empty($downstreamResponse->tokensWithError())) {
-            Log::error('FCM in error', $downstreamResponse->tokensWithError());
+        $notificationBuilder = new PayloadNotificationBuilder($title);
+        $notificationBuilder->setBody($body)->setSound('default');
+
+        $dataBuilder = new PayloadDataBuilder();
+        $dataBuilder->addData($data);
+
+        $option = $optionBuilder->build();
+        $notification = $notificationBuilder->build();
+        $data = $dataBuilder->build();
+
+        // You must change it to get your tokens
+        $tokens = Member::select('device_token');
+
+        if (!empty($user)) {
+            $tokens = $tokens->whereIn('id', $user);
         }
-        //        $downstreamResponse->tokensWithError();
+
+        if (!empty($exclude_user)) {
+            $tokens = $tokens->whereNotIn('id', $exclude_user);
+        }
+
+        $tokens = $tokens->whereNotNull('device_token')->where(
+            'device_token',
+            '!=',
+            ''
+        )->groupBy('device_token')->get()->pluck('device_token')->toArray();
+
+        if (!empty($tokens)) {
+            Log::info('FCM total token count', [count($tokens)]);
+            $downstreamResponse = FCM::sendTo($tokens, $option, $notification, $data);
+            Log::info('FCM in number of success', [$downstreamResponse->numberSuccess()]);
+            Log::error('FCM in number of failure', [$downstreamResponse->numberFailure()]);
+            $downstreamResponse->numberModification();
+
+            // return Array - you must remove all this tokens in your database
+            Member::whereIn('device_token', $downstreamResponse->tokensToDelete())->update(['device_token' => null]);
+
+            // return Array (key : oldToken, value : new token - you must change the token in your database)
+            $downstreamResponse->tokensToModify();
+
+            // return Array - you should try to resend the message to the tokens in the array
+            if (!empty($downstreamResponse->tokensToRetry())) {
+                FCM::sendTo($downstreamResponse->tokensToRetry(), $option, $notification, $data);
+            }
+
+            // return Array (key:token, value:error) - in production you should remove from your database the tokens present in this array
+            if (!empty($downstreamResponse->tokensWithError())) {
+                Log::error('FCM in error', $downstreamResponse->tokensWithError());
+            }
+            //        $downstreamResponse->tokensWithError();
+        }
+        Log::info('FCM end');
+    } catch (\Throwable $e) {
+        Log::error('FCM notification failed', [
+            'message' => strip_tags($e->getMessage()),
+        ]);
     }
-    Log::info('FCM end');
 }
 
 function checkDeferenceDeleteMedia($new, $old): void
