3067799a9e2b8862e21dbd68bc2c556322c23532
Sunil Solanki Mac <solankisunil537@gmail.com>
2026-06-16 11:19:39 +0530

    Refactor addUserLogin call in update method to pass Member model instance instead of array

--- app/Http/Controllers/Api/MemberController.php
+++ app/Http/Controllers/Api/MemberController.php
@@ -176,7 +176,7 @@ class MemberController extends ApiController
             Member::find($id)->fill($updatedFiled)->save();
 
             $iRes = Member::find($id);
-            $this->addUserLogin($iRes->toArray());
+            $this->addUserLogin($iRes);
 
             DB::commit();
 
